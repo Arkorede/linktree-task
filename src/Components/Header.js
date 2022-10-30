db675828915c443ca0ae556cdc93835f037fc77e
arkorede <israelashaolu2000@gmail.com>
2022-10-29 18:54:04 -0700

    Add alt attribute

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -8,7 +8,7 @@ function Header() {
       <img src={myImage} id="profile__img" alt="Israel Ashaolu" />
       <h1 id='twitter'>Israel Ashaolu</h1>
       <h1 id="slack">Arkorede</h1>
-      <img src={shareButton} className="btn-share" />
+      <img src={shareButton} className="btn-share" alt="A share button" />
     </header>
   </div >
 }
