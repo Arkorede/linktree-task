137e5ca16d6b3069be18d1315adb1716036e59c5
arkorede <israelashaolu2000@gmail.com>
2022-11-05 02:08:08 -0700

    feat: Contact Page

--- src/Routes/Contact.js
+++ src/Routes/Contact.js
@@ -1,27 +1,37 @@
 import React from 'react'
 
 function Contact() {
-  return <div>
-    <p class="lead">
-      This is a lead paragraph. It stands out from regular paragraphs.
+  return <div style={{ marginTop: "5.2rem" }}>
+    <h1>Contact Me</h1>
+    <p class="lead mb-5">
+      Hi there, contact me to ask me about anything you have in mind.
     </p>
     <form>
-      <div class="mb-3">
-        <label for="exampleInputEmail1" class="form-label">Email address</label>
-        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+      <div className='full-name'>
+        <div className="mb-3">
+          <label for="exampleInputFirstName1" className="form-label">First Name</label>
+          <input type="text" className="form-control" placeholder="Enter your first name" id="first_name" aria-describedby="firstNameHelp" required />
+        </div >
+        <div className="mb-3">
+          <label for="exampleInputLastName1" className="form-label">Last Name</label>
+          <input type="text" className="form-control" placeholder="Enter your last name" id="last_name" aria-describedby="lastNameHelp" required />
+        </div >
       </div>
       <div class="mb-3">
-        <label for="exampleInputPassword1" class="form-label">Password</label>
-        <input type="password" class="form-control" id="exampleInputPassword1" />
+        <label for="exampleInputEmail1" className="form-label">Email address</label>
+        <input type="email" className="form-control" placeholder="yourname@email.com" id="email" aria-describedby="emailHelp" required />
+      </div >
+      <div className="mb-3">
+        <label for="floatingTextarea2" className="form-label">Message</label>
+        <textarea className="form-control" placeholder="Send me a message and I'll reply you as soon as possible..." id="message" style={{ height: "100px" }} required></textarea>
       </div>
-      <div class="mb-3 form-check">
-        <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+      <div className="mb-3 form-check">
+        <input type="checkbox" class="form-check-input" id="exampleCheck1" required />
+        <label className="form-check-label" for="exampleCheck1">You agree to providing your data to Israel Ashaolu who may contact you.</label>
       </div>
-      <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
-  </div>
+      <button type="submit" className="btn btn-primary w-100" id="btn__submit" style={{ marginTop: "1rem", marginBottom: "8rem" }}>Submit</button>
+    </form >
+  </div >
 }
 
 export default Contact;
