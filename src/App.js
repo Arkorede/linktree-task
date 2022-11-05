2b1d274ffd6f53ad4a99b94a66cde38a6ac236e7
arkorede <israelashaolu2000@gmail.com>
2022-11-04 19:03:03 -0700

    refactor: The header component is now in the Content component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
-import './App.css';
+import './App.css'
 import './queries.css'
-import Header from './Components/Header'
-import Content from './Components/Content';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import Content from './Components/Content'
 import Footer from './Components/Footer'
+import Contact from './Routes/Contact'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Content />
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        {/* <Header /> */}
+        {/* <Content /> */}
+        {/* <Footer /> */}
+
+        <Routes>
+          <Route path="/" element={<Content />} />
+          {/* <Route path="/" element={<Footer />} /> */}
+          <Route exact path="/contact" element={<Contact />} />
+          <Route exact path="/redirect" element={<Navigate to="/contact" />} />
+          {/* <Redirect to="/contact" /> */}
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
--- src/Components/Content.js
+++ src/Components/Content.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import myImage from './myImage.jpg'
+import shareButton from './shareButton.png'
+import dotsButton from './dotsButton.png'
 import slack from './slack.png'
 import Icon from './Icon.png'
+import { Link } from 'react-router-dom'
 
 function Content() {
-  return <section>
+  return <div>
+    <div className="header">
+      <img src={myImage} id="profile__img" alt="Israel Ashaolu" />
+      <p id='twitter'>Israel Ashaolu</p>
+      <p id="slack">Arkorede</p>
+      <img src={shareButton} className="btn-share" alt="A share button" />
+      <img src={dotsButton} className="btn-dots" alt="A dots button" />
+    </div>
+
     <div className="text-links">
       <a href="https://twitter.com/IsraelAshaolu" id="twitter__link" className="text-link">Twitter Link</a>
       <a href="https://training.zuri.team/" id="btn__zuri" className="text-link">Zuri Team</a>
@@ -11,7 +23,7 @@ function Content() {
       <a href="https://books.zuri.team/python-for-beginners?ref_id=<arkorede>" id="book__python" title="Buy Python for Beginner😊" className="text-link">Python Books</a>
       <a href="https://background.zuri.team/" id="pitch" title="Where you pitch a service for doing background checks on coders" className="text-link">Background Check for Coders</a>
       <a href="https://books.zuri.team/design-rules" id="book__design" title="Where you pitch the free design book offered by Zuri." className="text-link">Design Books</a>
-      <a href="https://books.zuri.team/design-rules" id="contact" className="text-link">Contact Me</a>
+      <Link to="/contact" id="contact" className="text-link">Contact Me</Link>
     </div>
     <br />
     <br />
@@ -28,7 +40,7 @@ function Content() {
     <br />
     <br />
     <br />
-  </section >
+  </div>
 }
 
 export default Content;
